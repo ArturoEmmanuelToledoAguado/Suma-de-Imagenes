0e2b63940088035d7f8523142b718f22fe81d234
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-09-07 18:27:21 -0500

    Fin de la practica

--- SumImg.m
+++ SumImg.m
@@ -9,10 +9,10 @@ img2=imread('IMG/2.jpg');
 %Mostramos originales
 figure()
 imshow(img1)
-title('Imagen Original')
+title('Imagen Original 1')
 figure()
 imshow(img2)
-title('Imagen Original')
+title('Imagen Original 2')
 
 %Tamanos
 [f1 c1]=size(img1);
@@ -24,9 +24,54 @@ if (f1<f2) || (c1/3<c2/3)
     figure()
     imshow(img1r)
     title('Imagen Redimensionada')
+    %Mostramos las 2 formas
+    figure()
+    %A mano
+    imgSum=SImg(img2,img1r);
+    imgSum2=SImg(img1r,img2);
+    subplot(2,2,1)
+    imshow(imgSum)
+    title('Original 2 con Redimension de 1')
+    subplot(2,2,2)
+    imshow(imgSum2)
+    title('Redimension de 1 con Original 2')
+    %Funcion imadd
+    imgSum3=imadd(img2,img1r);
+    imgSum4=imadd(img1r,img2);
+    subplot(2,2,3)
+    imshow(imgSum3)
+    title('imadd(Original 2, Redimension 1)')
+    subplot(2,2,4)
+    imshow(imgSum4)
+    title('imadd(Redimension 1,Original 2)')
 else
     img2r=imresize(img2, [f1 c1/3]);
     figure()
     imshow(img2r)
     title('Imagen Redimensionada')
+    %Mostramos las 2 formas
+    figure()
+    %A mano
+    imgSum=SImg(img2r,img1);
+    imgSum2=SImg(img1,img2r);
+    subplot(2,2,1)
+    imshow(imgSum)
+    title('Redimension de 2 con Original 1')
+    subplot(2,2,2)
+    imshow(imgSum2)
+    title('Original 1 con Redimension de 2')
+    %Funcion imadd
+    imgSum3=imadd(img2r,img1);
+    imgSum4=imadd(img1,img2r);
+    subplot(2,2,3)
+    imshow(imgSum3)
+    title('imadd(Redimension 2, Original 1)')
+    subplot(2,2,4)
+    imshow(imgSum4)
+    title('imadd(Original 1, Redimension 2)')
+end
+
+%Suma a manita
+function imgSum = SImg(img1,img2)
+    imgSum=img1+img2;
 end
