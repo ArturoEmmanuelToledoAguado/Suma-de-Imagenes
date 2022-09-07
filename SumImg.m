7a28ceb9a7d35b3af9475b180c74ee3f8a55ff43
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-09-07 18:09:34 -0500

    Comparacion de tamanos

--- SumImg.m
+++ SumImg.m
@@ -5,3 +5,28 @@ close all;
 %Extraccion  de la imagenes
 img1=imread('IMG/1.jpg');
 img2=imread('IMG/2.jpg');
+
+%Mostramos originales
+figure()
+imshow(img1)
+title('Imagen Original')
+figure()
+imshow(img2)
+title('Imagen Original')
+
+%Tamanos
+[f1 c1]=size(img1);
+[f2 c2]=size(img2);
+
+%Comparación para reajuste
+if (f1<f2) || (c1/3<c2/3)
+    img1r=imresize(img1,[f2 c2/3]);
+    figure()
+    imshow(img1r)
+    title('Imagen Redimensionada')
+else
+    img2r=imresize(img2, [f1 c1/3]);
+    figure()
+    imshow(img2r)
+    title('Imagen Redimensionada')
+end
